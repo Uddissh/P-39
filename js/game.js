86e69c236ccf815b02ac7f8fc54d8dbdf865899a
Uddissh <68012232+Uddissh@users.noreply.github.com>
2020-12-21 18:04:47 +0530

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -87,13 +87,13 @@ class Game{
                      switch(rand){
                          case 1: fruits.addImage("fruit1",fruit1_img);
                          break;
-                         case 2: fruits.addImage("fruit1", fruit2_img);
+                         case 2: fruits.addImage("fruit2", fruit2_img);
                          break;
-                         case 3: fruits.addImage("fruit1", fruit3_img);
+                         case 3: fruits.addImage("fruit3", fruit3_img);
                          break;
-                         case 4: fruits.addImage("fruit1", fruit4_img);
+                         case 4: fruits.addImage("fruit4", fruit4_img);
                          break;
-                         case 5: fruits.addImage("fruit1", fruit5_img);
+                         case 5: fruits.addImage("fruit5", fruit5_img);
                          break;
                      }
                      fruitGroup.add(fruits);
@@ -102,6 +102,7 @@ class Game{
                  
                   if (player.index !== null) {
                      //fill code here, to destroy the objects.
+                     fruits.isTouching(players);
                   }
                 
 
